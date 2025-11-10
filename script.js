4db9573b86d5a53b7983371e73b0b4c1b816b04f
danielsousaazevdo <daniel.sousaazevdo@gmail.com>
2025-11-10 12:01:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function verificar() {
-    const n = Number(document.getElementByid('num').value)
+    const n = Number(document.getElementById('num').value)
     const dias = ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"]
-    document.getElementByid('res').innerText = 
+    document.getElementById('res').innerText = 
     n >= 1 && n <= 7 ? `Esse dia é ${dias[n - 1]}` : "Número inválido"
 }
